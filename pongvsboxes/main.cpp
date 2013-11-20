50ebc63b82f3339332e81244c68dd10c0fa3f38e
Pontus <ponor358@student.liu.se>
2013-11-20 16:52:20 +0100

    Spelskelett inlagt

--- pongvsboxes/main.cpp
+++ pongvsboxes/main.cpp
@@ -1,33 +1,12 @@
 #include <SFML/Graphics.hpp>
-#include "Round.h"
-#include <iostream>
+#include "Game.h"
 
 
 int main(int argc, char *argv[])
 {
+    //Initialize a game object and run it
+    Game game;
+    game.run();
 
-	// Create a new Game Round
-	Round* round = new Round();
-
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-	std::cout << "Testar att skriva ut test!" << std::endl;
-    //En ny kommentar
-	//En kommentar
+    return 0;
 }
