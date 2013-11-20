3011fccdc746f5d40ef048c1106b24258a9386c5
Erik Hammarlund <eriha891@gmail.com>
2013-11-20 14:32:21 +0100

    testar utskrift i main-filen

--- pongvsboxes/main.cpp
+++ pongvsboxes/main.cpp
@@ -1,8 +1,12 @@
 #include "Round.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
 
 	// Create a new Game Round
 	Round* round = new Round();
+
+
+	std::cout << "Testar att skriva ut test!" << endl;
 }
