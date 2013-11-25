b103ec210c05c7373216b26a5e8348452bc38049
Christoffer <chren574@student.liu.se>
2013-11-25 17:02:38 +0100

    Added the class block

---
+++ pongvsboxes/block.cpp
@@ -0,0 +1,21 @@
+#include "block.h"
+
+// Default constructor
+block::block()
+{
+    x = y = z = 0.0f;
+    lives = widthB = heightB = 0;
+}
+
+// Construcor to set position, size of block and lives
+block::block(float a, float b, float c, int l, int wB, int hB)
+{
+    x = a;
+    y = b;
+    z = c;
+
+    widhtB = wB;
+    heightB = hB;
+
+    lives = l;
+}
---
+++ pongvsboxes/block.h
@@ -0,0 +1,16 @@
+#ifndef BLOCK_H_INCLUDED
+#endif  BLOCK_H_INCLUDED
+
+class block
+{
+    public:
+
+        int lives, widthB, heightB;
+        float x, y, z;
+        // Default constructor
+        block();
+
+        block(float a, float b, float c);
+};
+
+#endif // CLOCK_H_INCLUDED
